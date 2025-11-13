5ce1e153d5ad2fab6f671f4ecae961f96b61cab4
hdoro <opensource@hdoro.dev>
2025-11-13 19:47:25 -0300

    start of confect integration

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,14 +1,48 @@
-import { defineSchema, defineTable } from 'convex/server'
-import { v } from 'convex/values'
+import { defineSchema, defineTable, Id } from '@rjdellecese/confect/server'
+import { Schema } from 'effect'
 
-export default defineSchema({
-	products: defineTable({
-		title: v.string(),
-		imageId: v.string(),
-		price: v.number(),
-	}),
-	todos: defineTable({
-		text: v.string(),
-		completed: v.boolean(),
-	}),
+type Tag = {
+	readonly name: string
+	readonly tags: readonly Tag[]
+}
+
+const Tag = Schema.Struct({
+	name: Schema.String,
+	tags: Schema.Array(Schema.suspend((): Schema.Schema<Tag> => Tag)),
 })
+
+export const confectSchema = defineSchema({
+	notes: defineTable(
+		Schema.Struct({
+			userId: Schema.optional(Id.Id('users')),
+			text: Schema.String.pipe(Schema.maxLength(100)),
+			tag: Schema.optional(Schema.String),
+			author: Schema.optional(
+				Schema.Struct({
+					role: Schema.Literal('admin', 'user'),
+					name: Schema.String,
+				}),
+			),
+			embedding: Schema.optional(Schema.Array(Schema.Number)),
+		}),
+	)
+		.index('by_text', ['text'])
+		.index('by_role', ['author.role'])
+		.searchIndex('text', {
+			searchField: 'text',
+			filterFields: ['tag'],
+		})
+		.vectorIndex('embedding', {
+			vectorField: 'embedding',
+			filterFields: ['author.name', 'tag'],
+			dimensions: 1536,
+		}),
+	users: defineTable(
+		Schema.Struct({
+			username: Schema.String,
+		}),
+	),
+	tags: defineTable(Tag),
+})
+
+export default confectSchema.convexSchemaDefinition
